128062bc4e57c2a0095bf1d0ddd94572b3843f8e
Jon Holmes <holmjona@isu.edu>
2019-02-13 12:21:06 -0700

    DOM stuff some more.

--- DemoSite/scripts/dom.js
+++ DemoSite/scripts/dom.js
@@ -1,3 +1,53 @@
-﻿var objList = document.getElementById("objectList");
+﻿//var objList = document.getElementById("objectList");
 
-alert(objList.innerText);
+//alert(objList.innerText);
+
+function moveToGroup1(obj) {
+    youveGotToMoveItMoveIt(obj, "listOne");
+}
+
+function moveToGroup2(obj) {
+    youveGotToMoveItMoveIt(obj, "listTwo");
+
+}
+
+function moveToGroup3(obj) {
+    youveGotToMoveItMoveIt(obj, "listThree");
+
+}
+
+
+function youveGotToMoveItMoveIt(obj, listName) {
+    //var owner = arguments[0];
+    var dMove = document.getElementById("divMove");
+    //alert(dMove.children.length);
+    var lOne = document.getElementById(listName);
+    //var tNode = window.document.createTextNode("Wagon");
+    var liNode = document.createElement("li");
+    //liNode.innerText = "Wagon";
+    //liNode.appendChild(tNode);
+    var parNode = obj.parentNode;
+    var uncNode;// = getPrevious(parNode);
+    uncNode = parNode.previousElementSibling;
+    // alert(uncNode.innerText + " | " + uncNode.innerHTML);
+
+    if (uncNode.children[0].tagName === "FORM") {
+        var txt = document.getElementById("txtInput");
+        liNode.innerText = txt.value;
+
+    } else {
+        liNode.innerHTML = uncNode.innerHTML;
+    }
+    //liNode.innerText = uncNode.innerText;
+    //liNode.appendChild(uncNode.children[0])
+
+    lOne.appendChild(liNode);
+}
+
+function getPrevious(o) {
+    var p = o.previousSibling;
+    if (o.NodeType === NodeType.TEXT) {
+        return getPrevious(p.previousSibling);
+    } else
+        return p;
+}
