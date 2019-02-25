143d4f9170a10f518b9a22026b94e8334bee9352
Jon Holmes <holmjona@isu.edu>
2019-02-25 12:13:36 -0700

    Finished working on the drag and drop code.

--- DemoSite/scripts/events.js
+++ DemoSite/scripts/events.js
@@ -1,4 +1,4 @@
-﻿
+﻿var liClickedElement; 
 
 // All event handlers should have an event object (e)
 function liClicked(e) {
@@ -10,8 +10,36 @@ function liClicked(e) {
     var trgt = e.target || e.srcElement;
 
     if (trgt.tagName === "LI") {
-        trgt.className = "picked";
+       // trgt.className = "picked";
     }
+}
+
+function liOtherClick(e) {
+    e = e || window.event;
+    //alert("Li was clicked");
+    this.className = "moveable";
+    liClickedElement = this;
+    setPostion(this, e);
+
+}
+function removeClick() {
+    liClickedElement = undefined;
+    this.className = "";
+}
+
+function liMoved(e) {
+    e = e || window.event;
+    //alert("Li was clicked");
+    trg = e.target || e.srcElement;
+    //if (trg.tagName === "LI")   setPostion(trg, e);
+    //if (trg === "LI") 
+    setPostion(liClickedElement, e);
+
+}
+
+function setPostion(ele, ev){
+    ele.style.top = (ev.y - 10) + "px";// "100px";
+    ele.style.left = (ev.x - 10) + "px";//"100px";
 }
 
 function liHovered(e) {
@@ -25,19 +53,52 @@ function liHovered(e) {
     }
 }
 
+function addToList(ul, txt) {
+    var ne = document.createElement("li");
+    ne.innerText = txt;
+    ul.appendChild(ne);
+}
+
+function setAddItemEvent(name) {
+    var red = document.getElementById(name);
+    red.onmouseenter = function () {
+        if (typeof liClickedElement !== "undefined") {
+            var ul = this.getElementsByTagName("ul")[0];
+            addToList(ul, liClickedElement.innerText);
+        }
+    };
+}
+
+
 window.onload = loader;
 
  function loader() {
     var liFroggie = document.getElementById("liFrog");
     //liFroggie.onclick = liClicked;
      var ulThingies = liFroggie.parentNode;
-     ulThingies.onclick = liClicked;
-     ulThingies.onmousemove = liHovered;
+     //ulThingies.onclick = liClicked;
+     //ulThingies.onmousemove = liHovered;
 
-     //var lis = document.getElementsByTagName("LI");
-     //for (var i = 0; i < lis.length; i++) {
+     var bdy = document.getElementsByTagName("body");
+     bdy[0].onmousemove = liMoved;
+
+     //var red = document.getElementById("divOne");
+     //red.onmouseenter = function () {
+     //    if (typeof liClickedElement !== "undefined") {
+     //        addToList(this, liClickedElement.innerText);
+     //    }
+     //};
+     setAddItemEvent("divOne");
+     setAddItemEvent("divTwo");
+     setAddItemEvent("divThree");
+
+     var lis = document.getElementsByTagName("LI");
+     for (var i = 0; i < lis.length; i++) {
+         lis[i].onmousedown = liOtherClick;
+         lis[i].onmouseup = removeClick;
+        //lis[i].onmousemove = liMoved;
      //    lis[i].onclick = liClicked;
      //    lis[i].onmousemove = liHovered;
-     //}
+     }
 
 }
