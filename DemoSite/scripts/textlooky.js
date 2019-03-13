38555987aba444cb7d497c4b086dcdd662b32d6e
Jon Holmes <holmjona@isu.edu>
2019-03-13 12:18:34 -0600

    Added jQuery code for accordian and tabs.

---
+++ DemoSite/scripts/textlooky.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(stuffAtStart);
+
+function stuffAtStart() {
+    $(".accordian,.tabs").click(function (e) {
+        e = e || window.event;
+        var t = e.target || e.srcElement;
+        var th = $(t);
+        //th.parent().find("h3").removeClass("selected");
+        th.siblings("h3.selected").removeClass("selected").next().slideUp(2000);
+        th.addClass("selected");
+        th.next().slideDown(2000, function () {
+            th.next().addClass("selected");
+            //alert("Boo");
+        });//.addClass("selected");
+       // th.prev().removeClass("selected");
+    });
+}
