900f3eef08610acdedb1af660bff46ae437f4503
Jon Holmes <holmjona@isu.edu>
2019-03-27 12:16:37 -0600

    Mores stuff about AJAX.

--- DemoSite/scripts/ajax.js
+++ DemoSite/scripts/ajax.js
@@ -27,13 +27,89 @@
                 //for (var i = 0; i < data.people.length; i++) {
                 //    alert(data.people[i].firstName);
                 //}
-                $.each(data.people, function (ndx,obj) {
-                    alert(obj.firstName);
+                var trh = $("<tr>");
+                trh.append($("<th>").text("First Name"));
+                trh.append($("<th>").text("Last Name"));
+                trh.append($("<th>").text("Age"));
+                $("#tblPeople").append(trh);
+                var count = 0;
+                $.each(data.people, function (ndx, obj) {
+                    //alert(obj.firstName);
+                    //var td = $("<td>").text(obj.firstName);
+
+                    //if (obj.age >= 18) {
+                        var tr = $("<tr>");
+                      //  if (count % 2 === 0) tr.addClass("other");
+                        tr.append($("<td>").text(obj.firstName));
+                        tr.append($("<td>").text(obj.lastName));
+                        tr.append($("<td>").text(obj.age));
+                        $("#tblPeople").append(tr);
+                        count++;
+                    //}
+                    //$("#tblPeople").append($("<tr>")
+                    //    .append($("<td>").text(obj.firstName)));
+                });
+            }
+        });
+    });
+
+    $("#btnAddRows").click(function () {
+        $.ajax({
+            url: "data/rows.html",
+            statusCode: {
+                404: function() {
+                    alert("File not found");
+                },
+                503: function () {
+                    alert("Server Error");
+                }
+            },
+            success: function (data) {
+                $("#tblCharacters").append(data);
+            },
+            error: function (o, e) {
+                alert("oops");
+            }
+        });
+    });
+
+    //$("#btnSendIt").click(function () {
+    //    $.ajax({
+    //        url: "http://teaching.jdholmes.net/postvget/reader/",
+    //        //data: ,
+    //        success: function (data) {
+    //            $("#divOutput").append(data);
+    //        },
+    //        error: function (o, e) {
+    //            alert("oops");
+    //        }
+
+    //    });
+    //});
+
+    $("#btnSearch").click(function () {
+        $.ajax({
+            url: "data/people.json",
+            //data: ,
+            success: function (data) {
+                $("#divOutput").empty();
+                $.each(data.addresses, function (ndx, obj) {
+                    var src = $("#txtSearch").val();
+                    var ans = obj.indexOf(src);
+                    if (ans >= 0) {
+                        $("#divOutput").append($("<p>").append(obj + "(" + ans + ")"));
+                    }
                 });
 
+                //$("#divOutput").append("I got the data");
+            },
+            error: function (o, e) {
+                alert("oops");
             }
 
         });
     });
+
+
 
 });
