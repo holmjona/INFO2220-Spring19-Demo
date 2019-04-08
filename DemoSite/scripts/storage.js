2c2853dcdf622f0c155b3900b98c04cd544d97c3
Jon Holmes <holmjona@isu.edu>
2019-04-08 12:17:41 -0600

    Added Jquery UI stuff.

--- DemoSite/scripts/storage.js
+++ DemoSite/scripts/storage.js
@@ -1,23 +1,66 @@
-﻿var cook = document.cookie;
-alert(cook);
+﻿//var cook = document.cookie;
+//alert(cook);
 
-if (cook === "") {
-    //var d = new Date();
-    //d.setTime(d.getTime() + 1000*60*60*2); // add two seconds.
-    document.cookie = "color=red;";
-    document.cookie = "number=17;";
-    document.cookie = "user={'name':'bob','lastname':'smith'}";
-}
+//if (cook === "") {
+//    //var d = new Date();
+//    //d.setTime(d.getTime() + 1000*60*60*2); // add two seconds.
+//    document.cookie = "color=red;";
+//    document.cookie = "number=17;";
+//    document.cookie = "user={'name':'bob','lastname':'smith'}";
+//}
 
-document.cookie = "number=58;";
+//document.cookie = "number=58;";
 
-var cookafter = document.cookie;
-var cookieTray = cookafter.split(';');
-for (var i = 0; i < cookieTray.length; i++) {
-    var cVals = cookieTray[i].split('=');
-    if (cVals[0].trim() === "user") {
-        var u = JSON.parse(cVals[1]);
-        alert(u.name);
+//var cookafter = document.cookie;
+//var cookieTray = cookafter.split(';');
+//for (var i = 0; i < cookieTray.length; i++) {
+//    var cVals = cookieTray[i].split('=');
+//    if (cVals[0].trim() === "user") {
+//        var u = JSON.parse(cVals[1]);
+//        alert(u.name);
+//    }
+//}
+//alert(cookafter);
+
+$(document).ready(function () {
+    $("#ulUsers").click(function (e) {
+        e = e || window.event;
+        var t = e.target || e.srcElement;
+
+        // alert user's name.
+        //alert(t.tagName);
+        var par = t.parentNode;
+        var kids = $(t).parent().contents();//.children();
+        //var kid1 = kids[0];
+        var kid2 = kids.eq(0);
+        //alert(kid2.text().trim());
+        var name = kid2.text().trim();
+        //var kid3 = par.childNodes[0];
+        //alert(kid3.textContent.trim());
+
+        localStorage.setItem("username", name);
+        var color = kids.eq(1).text();
+        sessionStorage.setItem("color", color);
+
+        displayFromStorage();
+
+        // at the end;
+        e.preventDefault();
+        return false;
+    });
+
+    displayFromStorage();
+
+});
+
+function displayFromStorage() {
+    if (localStorage.getItem("username") !== null) {
+        $("#hName").text(localStorage.getItem("username"));
+        $("#hName").css("color", sessionStorage.getItem("color"));
     }
+
+    
+
 }
-alert(cookafter);
+
+
---
+++ DemoSite/scripts/uiscripts.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+    $(".accordian").accordion();
+    $(".tabs").tabs({ show: { effect: "blind", duration: 3000 } });
+    $("#listItems li").draggable({
+        classes: { "ui-sortable-helper": "selected" },
+        revert: true
+    });
+    $("#listOne").droppable({
+        accept: ".redThing",
+        drop: droppy
+    });
+
+    $("#listTwo").droppable({
+        accept: ".greenThing",
+        drop: droppy
+    });
+
+    $("#listThree").droppable({
+        accept: ".blueThing",
+        drop: droppy
+    });
+
+
+    function droppy(e, ui) {
+        var obj = ui.draggable[0];
+        $(this).append(obj);
+        obj.style = "";
+    }
+});
